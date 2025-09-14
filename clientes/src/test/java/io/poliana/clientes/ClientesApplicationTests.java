7182487985da3b40696f436566eb590de755a918
POLLY1515 <amarantepolly2030@gmail.com>
2025-09-14 14:10:54 -0300

    project created

---
+++ clientes/src/test/java/io/poliana/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.poliana.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ produtos/src/test/java/io/poliana/produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.poliana.produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
